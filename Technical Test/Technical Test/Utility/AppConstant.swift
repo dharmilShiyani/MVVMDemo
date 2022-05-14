8ffb17a2d931a42b19b9947a3b16e2206584f065
JSInfotech <jsinfotech117@gmail.com>
2022-05-14 09:52:01 +0530

    code push

---
+++ Technical Test/Technical Test/Utility/AppConstant.swift
@@ -0,0 +1,20 @@
+//
+//  AppConstant.swift
+//  Technical Test
+//
+//  Created by Dharmil on 13/05/22.
+//
+
+import Foundation
+
+// MARK: - Constant
+class AppConstant {
+    
+   
+    struct API {
+       
+        static let BASE_URL             = "https://jsonplaceholder.typicode.com/"
+
+        static let kPosts               = BASE_URL + "posts"
+    }
+}
